36e8dd93df551d92a013cd4241664dc041fbe94b
Raka Fajar Salinggih <73903697+rakafajars@users.noreply.github.com>
2023-07-20 23:22:16 +0700

    add style shadow

---
+++ lib/common/style.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+/// TODO TAMHBAHKAN COLOR SHADOW
+const List<BoxShadow> mdBoxShadow = [
+  BoxShadow(
+    blurRadius: 2,
+    offset: Offset(0, 2),
+    spreadRadius: 0,
+  ),
+  BoxShadow(
+    blurRadius: 3,
+    offset: Offset(0, 4),
+    spreadRadius: 0,
+  ),
+];
+
+
+const List<BoxShadow> lgBoxShadow = [
+  BoxShadow(
+    blurRadius: 8,
+    offset: Offset(0, 10),
+    spreadRadius: 0,
+  ),
+  BoxShadow(
+    blurRadius: 3,
+    offset: Offset(0, 4),
+    spreadRadius: 0,
+  ),
+];
+
+const List<BoxShadow> xlBoxShadow = [
+  BoxShadow(
+    blurRadius: 25,
+    offset: Offset(0, 25),
+    spreadRadius: 0,
+  ),
+]; 
+
+
+
+
