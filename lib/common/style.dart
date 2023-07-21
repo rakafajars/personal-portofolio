278a52a79b8cb57c78c31253136cc9521bcc504c
Raka Fajar Salinggih <73903697+rakafajars@users.noreply.github.com>
2023-07-21 22:26:00 +0700

    Tambah Style Color Global

--- lib/common/style.dart
+++ lib/common/style.dart
@@ -51,4 +51,19 @@ class ColorsStyle {
   Color grayLight800 = const Color(0xff1f2937);
   Color grayLight900 = const Color(0xff111827);
   Color grayLight950 = const Color(0xff030712);
+
+  Color grayDarkDefault = const Color(0xff030712);
+  Color grayDark50 = const Color(0xff111827);
+  Color grayDark100 = const Color(0xff1f2937);
+  Color grayDark200 = const Color(0xff374151);
+  Color grayDark300 = const Color(0xff4b5563);
+  Color grayDark400 = const Color(0xff6b7280);
+  Color grayDark500 = const Color(0xff9ca3af);
+  Color grayDark600 = const Color(0xffd1d5db);
+  Color grayDark700 = const Color(0xffe5e7eb);
+  Color grayDark800 = const Color(0xfff3f4f6);
+  Color grayDark900 = const Color(0xfff9fafb);
+  Color grayDark950 = const Color(0xffffffff);
+
+  Color emerald = const Color(0xff10b981);
 }
