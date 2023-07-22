d7729e42a129a24a78d7fd5139bb11323640d2fe
Raka Fajar Salinggih <73903697+rakafajars@users.noreply.github.com>
2023-07-22 16:59:59 +0700

    memisahkan file common style,size,color

--- lib/common/common_style.dart
+++ lib/common/common_style.dart
@@ -1,77 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:portofolio/common/common_color.dart';
 import 'package:portofolio/common/common_size.dart';
 
-class CommonShadow {
-  List<BoxShadow> mdBoxShadow = [
-    const BoxShadow(
-      color: Color(0x0F000000),
-      blurRadius: 2,
-      offset: Offset(0, 2),
-      spreadRadius: 0,
-    ),
-    const BoxShadow(
-      color: Color(0x11000000),
-      blurRadius: 3,
-      offset: Offset(0, 4),
-      spreadRadius: 0,
-    ),
-  ];
 
-  List<BoxShadow> lgBoxShadow = [
-    const BoxShadow(
-      color: Color(0x0A000000),
-      blurRadius: 8,
-      offset: Offset(0, 10),
-      spreadRadius: 0,
-    ),
-    const BoxShadow(
-      color: Color(0x19000000),
-      blurRadius: 3,
-      offset: Offset(0, 4),
-      spreadRadius: 0,
-    ),
-  ];
-
-  List<BoxShadow> xlBoxShadow = [
-    const BoxShadow(
-      color: Color(0x26000000),
-      blurRadius: 25,
-      offset: Offset(0, 25),
-      spreadRadius: 0,
-    ),
-  ];
-}
-
-class CommonColor {
-  Color grayLightDefault = const Color(0xffffffff);
-  Color grayLight50 = const Color(0xfff9fafb);
-  Color grayLight100 = const Color(0xfff3f4f6);
-  Color grayLight200 = const Color(0xffe5e7eb);
-  Color grayLight300 = const Color(0xffd1d5db);
-  Color grayLight400 = const Color(0xff9ca3af);
-  Color grayLight500 = const Color(0xff6b7280);
-  Color grayLight600 = const Color(0xff4b5563);
-  Color grayLight700 = const Color(0xff374151);
-  Color grayLight800 = const Color(0xff1f2937);
-  Color grayLight900 = const Color(0xff111827);
-  Color grayLight950 = const Color(0xff030712);
-
-  Color grayDarkDefault = const Color(0xff030712);
-  Color grayDark50 = const Color(0xff111827);
-  Color grayDark100 = const Color(0xff1f2937);
-  Color grayDark200 = const Color(0xff374151);
-  Color grayDark300 = const Color(0xff4b5563);
-  Color grayDark400 = const Color(0xff6b7280);
-  Color grayDark500 = const Color(0xff9ca3af);
-  Color grayDark600 = const Color(0xffd1d5db);
-  Color grayDark700 = const Color(0xffe5e7eb);
-  Color grayDark800 = const Color(0xfff3f4f6);
-  Color grayDark900 = const Color(0xfff9fafb);
-  Color grayDark950 = const Color(0xffffffff);
-
-  Color emerald = const Color(0xff10b981);
-}
 
 class CommonStyle {
 
