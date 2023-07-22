108bd25c33ec98f1624631bf2a8a3b6d98d754a7
Raka Fajar Salinggih <73903697+rakafajars@users.noreply.github.com>
2023-07-22 18:38:22 +0700

    add icon style

--- lib/common/common_style.dart
+++ lib/common/common_style.dart
@@ -3,21 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:portofolio/common/common_color.dart';
 import 'package:portofolio/common/common_size.dart';
 
-
-
 class CommonStyle {
-
-  /// TODO COBA CEK KEMBALI LAGI, HARUSNYA SEPERTI APA FONT GLOBAL
-  // FontWeight weight700 = FontWeight.w700;
-  // FontWeight weight600 = FontWeight.w600;
-  // FontWeight weight500 = FontWeight.w500;
-  // FontWeight weight400 = FontWeight.w400;
-
-
   /// TODO BAGAIMANA CARA MEMBUAT HEADING 1 BISA, KE WEB, MOBILE , TABLET DAN DESKTOP
   /// DENGAN UKURAN YANG BERBEDA
 
-  /// HEADING Desktop 1
   TextStyle heading1 = GoogleFonts.inter(
     fontWeight: FontWeight.w700,
     fontSize: CommonSize().size60,
@@ -27,10 +16,80 @@ class CommonStyle {
 
   TextStyle heading2 = GoogleFonts.inter(
     fontWeight: FontWeight.w600,
+    fontSize: CommonSize().size36,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
   );
 
-  
-  TextStyle heading3 = GoogleFonts.inter(
+  TextStyle heading3SemiBold = GoogleFonts.inter(
     fontWeight: FontWeight.w600,
+    fontSize: CommonSize().size30,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+  );
+
+  TextStyle heading3Bold = GoogleFonts.inter(
+    fontWeight: FontWeight.w700,
+    fontSize: CommonSize().size30,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+  );
+
+  TextStyle subtitleNormal = GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
+    fontSize: CommonSize().size20,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+  );
+
+  TextStyle subtitleSemiBold = GoogleFonts.inter(
+    fontWeight: FontWeight.w600,
+    fontSize: CommonSize().size20,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+  );
+
+  TextStyle body1 = GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
+    fontSize: CommonSize().size18,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+  );
+
+  TextStyle body2Normal = GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
+    fontSize: CommonSize().size16,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+  );
+
+  TextStyle body2UnderLine = GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
+    fontSize: CommonSize().size16,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+    decoration: TextDecoration.underline,
+  );
+
+  TextStyle body2Medium = GoogleFonts.inter(
+    fontWeight: FontWeight.w500,
+    fontSize: CommonSize().size16,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+    decoration: TextDecoration.underline,
+  );
+
+  TextStyle body3 = GoogleFonts.inter(
+    fontWeight: FontWeight.w400,
+    fontSize: CommonSize().size14,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
+  );
+
+  TextStyle body3Medium = GoogleFonts.inter(
+    fontWeight: FontWeight.w500,
+    fontSize: CommonSize().size14,
+    letterSpacing: -1.20,
+    color: CommonColor().grayLight900,
   );
 }
