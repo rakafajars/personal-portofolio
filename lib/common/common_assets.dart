4e47568698a5c7c9677cb68824225d1cb5a368dd
raka <rakafajar18@gmail.com>
2023-07-23 10:30:36 +0700

    fix icon generator

---
+++ lib/common/common_assets.dart
@@ -0,0 +1,15 @@
+class AssetIcon {
+	static const String iconLightClose = 'assets/icon/icon_light_close.svg';
+	static const String iconLightCopy = 'assets/icon/icon_light_copy.svg';
+	static const String iconLightCopyRight = 'assets/icon/icon_light_copy_right.svg';
+	static const String iconLightDarkMode = 'assets/icon/icon_light_dark_mode.svg';
+	static const String iconLightExternalLink = 'assets/icon/icon_light_external_link.svg';
+	static const String iconLightGithub = 'assets/icon/icon_light_github.svg';
+	static const String iconLightInbox = 'assets/icon/icon_light_inbox.svg';
+	static const String iconLightLocation = 'assets/icon/icon_light_location.svg';
+	static const String iconLightMenu = 'assets/icon/icon_light_menu.svg';
+	static const String iconLightMode = 'assets/icon/icon_light_mode.svg';
+	static const String iconLightPhone = 'assets/icon/icon_light_phone.svg';
+	static const String iconLightTwitter = 'assets/icon/icon_light_twitter.svg';
+}
+
