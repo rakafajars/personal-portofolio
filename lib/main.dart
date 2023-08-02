e2a70f4dccda428db8237e1c5d94b4984f67d5ba
raka <rakafajar18@gmail.com>
2023-08-02 20:26:38 +0700

    add hero banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:portofolio/common/common_assets.dart';
 import 'package:portofolio/common/common_style.dart';
 
 import 'persentation/pages/portofolio/header_portofolio.dart';
@@ -38,6 +39,55 @@ class MyHomePage extends StatelessWidget {
       appBar: PreferredSize(
         preferredSize: Size(screenSize.width, 1000),
         child: const HeaderPortofolio(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              color: Colors.transparent,
+              padding:
+                  const EdgeInsets.symmetric(vertical: 106, horizontal: 112),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hi, I’m Raka 👋",
+                          style: CommonStyle().heading1,
+                        ),
+                        Text(
+                          "I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.",
+                          style: CommonStyle().body2Normal,
+                          textAlign: TextAlign.justify,
+                        ),
+                        const SizedBox(
+                          height: 48,
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              AssetIcon.iconLightLocation,
+                            ),
+                            Text('Bandung, Indonesia')
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 128),
+                  const Expanded(
+                    child: Placeholder(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/persentation/pages/portofolio/header_portofolio.dart
+++ lib/persentation/pages/portofolio/header_portofolio.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:portofolio/common/common_assets.dart';
-import 'package:portofolio/common/common_color.dart';
+
 import 'package:portofolio/common/common_size.dart';
 import 'package:portofolio/common/common_style.dart';
 import 'package:portofolio/data/models/header_model.dart';
-import 'package:portofolio/persentation/widget/button_widget.dart';
+import 'package:portofolio/persentation/pages/portofolio/widget/header_widget.dart';
 
 class HeaderPortofolio extends StatelessWidget {
   const HeaderPortofolio({super.key});
@@ -32,51 +30,6 @@ class HeaderPortofolio extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class HeaderWidget extends StatelessWidget {
-  final List<HeaderModel> headerModels;
-  const HeaderWidget({
-    super.key,
-    required this.headerModels,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Row(
-          children: headerModels
-              .map(
-                (data) => Padding(
-                  padding: const EdgeInsets.only(right: 24),
-                  child: Text(
-                    data.name,
-                    style: CommonStyle().body2Medium.copyWith(
-                          color: CommonColor().grayLight600,
-                        ),
-                  ),
-                ),
-              )
-              .toList(),
-        ),
-        Container(
-          color: CommonColor().grayLight100,
-          height: 24,
-          width: 1,
-        ),
-        const SizedBox(width: 24),
-        SvgPicture.asset(
-          AssetIcon.iconLightMode,
-        ),
-        const SizedBox(width: 16),
-        ButtonWidget(
-          title: 'Download CV',
-          onTap: () {},
-        ),
-      ],
     );
   }
 }
